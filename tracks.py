36d5ce196ba11ab2b77ed3e8f74c07938ceb7cdb
palomachcc <110131341+palomachcc@users.noreply.github.com>
2022-09-13 16:36:35 -0300

    Add files via upload

---
+++ tracks.py
@@ -0,0 +1,110 @@
+#Musical Track Database  https://www.py4e.com/code3/tracks/Library.xml
+
+#This application will read an iTunes export file in XML and produce a properly normalized database. ejemplo: https://www.py4e.com/code3/tracks/tracks.py
+
+import xml.etree.ElementTree as ET
+import sqlite3
+
+conn = sqlite3.connect('tracksdb.sqlite')
+cur = conn.cursor()
+
+
+# Make some fresh tables using executescript()
+cur.executescript('''
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Genre;
+DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Track;
+
+CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Album (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id  INTEGER,
+    title   TEXT UNIQUE
+);
+
+CREATE TABLE Track (
+    id  INTEGER NOT NULL PRIMARY KEY
+        AUTOINCREMENT UNIQUE,
+    title TEXT  UNIQUE,
+    album_id  INTEGER,
+    genre_id  INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER
+);
+''')
+
+
+fname = input('Enter file name: ')
+if ( len(fname) < 1 ) : fname = 'Library.xml'
+
+
+
+
+# <key>Track ID</key><integer>369</integer>
+# <key>Name</key><string>Another One Bites The Dust</string>
+# <key>Artist</key><string>Queen</string>
+def lookup(d, key):
+    found = False
+    for child in d:
+        if found : return child.text
+        if child.tag == 'key' and child.text == key :
+            found = True
+    return None
+
+
+stuff = ET.parse(fname)
+all = stuff.findall('dict/dict/dict')
+#print('Dict count:', len(all))
+
+
+
+for entry in all:
+    #para cada elemento de all, quiero que realice lo siguiente:
+    if ( lookup(entry, 'Track ID') is None ) : continue
+    #continue significa que vuelve a retomar el loop, en este caso pasaria al siguiente elemento. The continue keyword is used to end the current iteration in a for loop (or a while loop), and continues to the next iteration.
+    name = lookup(entry, 'Name')
+    artist = lookup(entry, 'Artist')
+    genre = lookup(entry, 'Genre')
+    album = lookup(entry, 'Album')
+    count = lookup(entry, 'Play Count')
+    rating = lookup(entry, 'Rating')
+    length = lookup(entry, 'Total Time')
+
+    if name is None or genre is None or artist is None or album is None :
+        continue
+
+    #print(name, genre, artist, album, count, rating, length)
+
+
+    cur.execute('''INSERT OR IGNORE INTO Artist (name)
+        VALUES ( ? )''', ( artist, ) )
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre (name)
+        VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
+        VALUES ( ?, ? )''', ( album, artist_id ) )
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, genre_id, len, rating, count)
+        VALUES ( ?, ?, ?, ?, ?, ? )''',
+        ( name, album_id, genre_id, length, rating, count ) )
+
+    conn.commit()
+
+exit=input("press enter to exit")
